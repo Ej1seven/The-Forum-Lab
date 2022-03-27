0be414b8de0596a2b10ff43d1086edea320ec12e
Ej1seven <erikhunter@erikhunter.dev>
2022-03-27 10:54:54 -0500

    deployed app the vercel

--- forum-flow-web/src/pages/_app.tsx
+++ forum-flow-web/src/pages/_app.tsx
@@ -1,22 +1,28 @@
 //Chakra UI is a simple, modular and accessible component library that gives you the building blocks you need to build your React applications.
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 //urql is a highly customizable and versatile GraphQL client
 import '../styles/globals.css';
 import theme from '../theme';
 
+//Provider allows the application to make use of the GraphQl client
+//ChakraProvider allows the application to make use of Chakra UI
 function MyApp({ Component, pageProps }: any) {
   return (
-    //Provider allows the application to make use of the GraphQl client
-    //ChakraProvider allows the application to make use of Chakra UI
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>The Forum Lab</title>
+      </Head>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
--- forum-flow-web/src/pages/_document.tsx
+++ forum-flow-web/src/pages/_document.tsx
@@ -1,12 +1,12 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
-
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="preconnect" href={'https://fonts.googleapis.com'} />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
